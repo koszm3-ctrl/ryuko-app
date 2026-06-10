5b4bbcf59f2c5145799ee74ebe178aee0625d568
koszm3-ctrl <koszm3@gmail.com>
2026-06-09 21:28:44 -0600

    list

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2851427672" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1671435126" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
